5197d82e7a3205cea841db7e31d3c99f1d108d88
Canna03 <junkim1415@gmail.com>
2024-05-28 16:57:37 -0400

    roman to integer solution

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbol = ["I", "V", "X", "L", "C", "D", "M"]
+        value = [1, 5, 10, 50, 100, 500, 1000]
+        
+        highest = 6
+        total = 0
+        
+        for i in range(len(s)):
+            letter = symbol.index(s[i])
+            
+            if letter > highest + 1:
+                total += value[highest] * 8
+            elif highest < letter:
+                total += value[highest] * 3
+            else:
+                highest = letter
+                total += value[highest]
+                
+        return total
