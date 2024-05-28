fd2f54dd2455f943fc8a0d573f629029bace4c17
Canna03 <junkim1415@gmail.com>
2024-05-28 18:29:49 -0400

    longest common prefix

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ""
+        smallest_length = len(strs[0])
+        for word in strs:
+            if len(word) < smallest_length:
+                smallest_length = len(word)
+        
+        for letter in range(smallest_length):
+            for word in strs[1:]:
+                if word[letter] != strs[0][letter]:
+                    return prefix
+            prefix += strs[0][letter]
+        return prefix
+            
+        
+        
+
+test = Solution()
+print(test.longestCommonPrefix(["ab","a"]))
