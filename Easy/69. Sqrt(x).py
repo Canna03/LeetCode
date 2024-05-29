06be8172a4fa957a5447aecb353ee7d6bcdd35b8
Canna03 <junkim1415@gmail.com>
2024-05-29 11:21:11 -0400

    sqrt x

--- Easy/69. Sqrt(x).py
+++ Easy/69. Sqrt(x).py
@@ -1,3 +1,25 @@
+# 67.57% Runtime
+# 87.34% Memory
+
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return
+        
+        if x == 0:
+            return 0
+        if x < 4:
+            return 1
+        
+        def Binary_Search(target: int, low: int, high: int) -> int:
+            if low > high:
+                return -1
+            mid = (low + high) // 2
+            
+            if mid ** 2 == target or (mid ** 2 < target and (mid + 1) ** 2 > target):
+                return mid
+            if mid ** 2 < target:
+                return Binary_Search(target, mid + 1, high)
+            if mid ** 2 > target:
+                return Binary_Search(target, low, mid - 1)
+            return -1
+        
+        return Binary_Search(x, 1, x // 2)
