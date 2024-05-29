6614c7cd83111ab876375f8a7914536d158413dc
Canna03 <junkim1415@gmail.com>
2024-05-29 11:09:37 -0400

    plus one

--- Easy/66. Plus One.py
+++ Easy/66. Plus One.py
@@ -1,5 +1,17 @@
+# 93.33% Runtime
+# 83.09% Memory
+
 from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        return
+        if digits == [9]*len(digits):
+            return [1] + [0] * len(digits)
+        
+        index = len(digits) - 1
+        digits[index] += 1
+        while digits[index] == 10:
+            digits[index] = 0
+            index -= 1
+            digits[index] += 1
+        return digits
