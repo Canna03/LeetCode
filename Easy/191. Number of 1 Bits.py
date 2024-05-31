d6bdff6a861b890fcd5e3376e2f56724890e9d05
Canna03 <106220249+Canna03@users.noreply.github.com>
2024-05-31 15:06:46 -0400

    191

--- Easy/191. Number of 1 Bits.py
+++ Easy/191. Number of 1 Bits.py
@@ -1,3 +1,11 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        return
+        bit_count = 0
+        for i in range(32):
+            power = 2 ** (31 - i)
+            if n >= power:
+                n -= power
+                bit_count += 1
+        return bit_count
+    
+print(Solution().hammingWeight(11))
