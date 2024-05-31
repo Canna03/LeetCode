a29abe0e06bc04ca77ea82f19f3e6fe944d9261e
Canna03 <106220249+Canna03@users.noreply.github.com>
2024-05-31 15:00:45 -0400

    190

--- Easy/190. Reverse Bits.py
+++ Easy/190. Reverse Bits.py
@@ -1,3 +1,11 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        return
+        num = bin(n)[2:]
+        num = "0"*(32-len(num)) + num
+        reverse = 0
+        for i in range(len(num)):
+            print(num[i])
+            reverse += int(num[i]) * (2 ** i)
+        return reverse
+
+print(Solution().reverseBits(43261596))
