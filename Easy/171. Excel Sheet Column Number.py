ec9f7b31938ae24a67c835c4d2504e017c60cbf4
Canna03 <106220249+Canna03@users.noreply.github.com>
2024-05-31 14:46:38 -0400

    171

--- Easy/171. Excel Sheet Column Number.py
+++ Easy/171. Excel Sheet Column Number.py
@@ -1,3 +1,8 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        return
+        column = 0
+        for letter in columnTitle:
+            column *= 26
+            position = ord(letter) % 64
+            column += position
+        return column
