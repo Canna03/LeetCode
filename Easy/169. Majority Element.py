03df7b2bf56cfd8c7d2d987d35c34e8f58dd6db8
Canna03 <106220249+Canna03@users.noreply.github.com>
2024-05-31 14:42:56 -0400

    169

--- Easy/169. Majority Element.py
+++ Easy/169. Majority Element.py
@@ -2,4 +2,14 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return
+        count = 0
+        majority = nums[0]
+        for i in range(len(nums)):
+            if nums[i] == majority:
+                count += 1
+            else:
+                if count == 0:
+                    majority = nums[i]
+                else:
+                    count -= 1
+        return majority
