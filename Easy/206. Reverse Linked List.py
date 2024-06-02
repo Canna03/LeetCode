e31216cf2346832a874d73bb187ec111c9945563
Canna03 <junkim1415@gmail.com>
2024-06-01 23:04:38 -0400

    206 completed

--- Easy/206. Reverse Linked List.py
+++ Easy/206. Reverse Linked List.py
@@ -7,4 +7,15 @@ class ListNode:
         
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return
+        reverse_list = None
+        while head:
+            next_node = head.next
+            head.next = reverse_list
+            reverse_list = head
+            head = next_node
+        return reverse_list
+
+node = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+
+print(Solution().reverseList(node))
+            
