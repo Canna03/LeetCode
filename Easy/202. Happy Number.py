55785c324f4c753861875d11b0150a180725f784
Canna03 <junkim1415@gmail.com>
2024-06-01 22:44:17 -0400

    202 done

--- Easy/202. Happy Number.py
+++ Easy/202. Happy Number.py
@@ -1,5 +1,12 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        while n > 10:
+            square_sum = 0
+            for num in str(n):
+                square_sum += int(num) ** 2
+            n = square_sum
+        if n != 1 and n != 7 and n != 10:
+            return False
         return True
     
 print(Solution().isHappy(19))
